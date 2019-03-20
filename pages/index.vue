cc17049230e850b7eba5bb430a243b11b1ae3cfb
Divya Sasidharan <hello@shortdiv.com>
2019-03-20 11:10:43 -0500

    add page and asset

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,7 @@
         My bee&#39;s knees Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+        <nuxt-link to="/page">Page</nuxt-link>
       </div>
     </div>
   </section>
---
+++ pages/page.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="page">
+    <h1>Netlify loves Nuxt</h1>
+    <img src="../assets/netlify_logomark.png" alt="">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "page"
+  }
+</script>
+
+<style lang="scss" scoped>
+#page {
+  margin: 10em;
+
+  & h1 {
+    text-align: center;
+  }
+
+  & img {
+    display: block;
+    margin: 0 auto;
+    width: 100px;
+    margin-top: 1em;
+  }
+}
+</style>
